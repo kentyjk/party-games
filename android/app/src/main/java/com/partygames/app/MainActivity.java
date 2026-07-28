49c6b85a7cb10df0f9ec637b121121f758ffd716
Ken <kentyjk@users.noreply.github.com>
2026-07-28 02:12:06 +0000

    Revert MainActivity to simple BridgeActivity

--- android/app/src/main/java/com/partygames/app/MainActivity.java
+++ android/app/src/main/java/com/partygames/app/MainActivity.java
@@ -1,15 +1,5 @@
 package com.partygames.app;
 
 import com.getcapacitor.BridgeActivity;
-import java.util.Arrays;
-import java.util.List;
-import com.getcapacitor.PluginHandle;
-import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(PartyGamesPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
